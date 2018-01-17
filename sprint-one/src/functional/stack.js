592c264c149ec722d18f7fd6056733d7000379a0
waynekwest88@gmail.com <waynekwest88@gmail.com>
2018-01-17 15:21:20 -0800

    completed stack requirements

--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -6,12 +6,25 @@ var Stack = function() {
 
   // Implement the methods below
   someInstance.push = function(value) {
+    var size = Object.keys(storage).length;
+    storage[size] = value;
   };
 
   someInstance.pop = function() {
+    var size = Object.keys(storage).length - 1;
+    var value = storage[size];
+    delete storage[size];
+    return value;
   };
 
   someInstance.size = function() {
+    var size = Object.keys(storage).length;
+    return size;
+  };
+
+  someInstance.sequentialAdd = function(value) {
+    var savedValue = value;
+    return savedValue;
   };
 
   return someInstance;
