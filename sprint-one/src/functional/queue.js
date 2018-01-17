dc2c22a3fbf7704ba60d7c103b82c8c78326fc2a
waynekwest88@gmail.com <waynekwest88@gmail.com>
2018-01-17 15:24:24 -0800

    completed queue requirements

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -1,5 +1,6 @@
 var Queue = function() {
   var someInstance = {};
+  var dequeue = 0;
 
   // Use an object with numeric keys to store values
   var storage = {};
@@ -7,12 +8,26 @@ var Queue = function() {
   // Implement the methods below
 
   someInstance.enqueue = function(value) {
+    var size = Object.keys(storage).length + dequeue;
+    storage[size] = value;
   };
 
   someInstance.dequeue = function() {
+    dequeue++;
+    var smallestKey = Object.keys(storage).length;
+    for (var key in storage) {
+      if (key < smallestKey) {
+        smallestKey = key;
+      }
+    }
+    var value = storage[smallestKey];
+    delete storage[smallestKey];
+    return value;
   };
 
   someInstance.size = function() {
+    var size = Object.keys(storage).length;
+    return size;
   };
 
   return someInstance;
