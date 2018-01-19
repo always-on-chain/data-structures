0a9a7f49d2b15bab429ae5f18a4aa02168a3ed2a
waynekwest88@gmail.com <waynekwest88@gmail.com>
2018-01-18 16:28:20 -0800

    completed pseudoclassical - queue

--- sprint-one/src/pseudoclassical/queue.js
+++ sprint-one/src/pseudoclassical/queue.js
@@ -1,6 +1,28 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  this.countEnqueue = 0;
+  this.countDequeue = 0;
+  this.storage = {};
+};
+
+Queue.prototype.size = function() {
+  return this.countEnqueue - this.countDequeue;
+};
+
+Queue.prototype.enqueue = function(value) {
+  this.storage[this.countEnqueue] = value;
+  this.countEnqueue++;
+};
+
+Queue.prototype.dequeue = function() {
+  if (this.countDequeue === this.countEnqueue) {
+    return;
+  }
+  var elimatedValue = this.storage[this.countDequeue];
+  delete this.storage[this.countDequeue];
+  this.countDequeue++;
+  return elimatedValue;
 };
 
 
