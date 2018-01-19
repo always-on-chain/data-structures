ba8097daa47a6d908174344fe7a29e38e4a2d9c9
waynekwest88@gmail.com <waynekwest88@gmail.com>
2018-01-18 16:05:42 -0800

    completed prototypal - shared

--- sprint-one/src/prototypal/stack.js
+++ sprint-one/src/prototypal/stack.js
@@ -1,8 +1,32 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var obj = Object.create(stackMethods);
+  return obj;
 };
 
-var stackMethods = {};
+var stackMethods = {
+  count: 0,
+  storage: {}
+};
+
+stackMethods.size = function() {
+  return this.count;
+};
+
+stackMethods.push = function(value) {
+  this.count++;
+  this.storage[this.count] = value;
+};
+
+
+stackMethods.pop = function(value) {
+  if (this.count > 0) {
+    var poppedValue = this.storage[this.count];
+    delete this.storage[this.count];
+    this.count--;
+  }
+  return poppedValue;
+};
 
 
