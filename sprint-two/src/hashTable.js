00c6e80e9403274c1d1832d8f9bd3c0d2cbb266f
waynekwest88 <waynekwest88@gmail.com>
2018-01-20 20:02:47 -0800

    Completed HashTables

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -7,20 +7,33 @@ var HashTable = function() {
 
 HashTable.prototype.insert = function(k, v) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  var collArray = this._storage.get(index);
+  if (collArray === undefined) {
+    this._storage.set(index, []);
+    this._storage.get(index).push([k, v]);
+  } else if ( collArray[0][0] ===  k ) {
+    collArray[0][1] = v;
+    this._storage.set(index, collArray);
+  } else {
+    this._storage.get(index).push([k, v]);
+  }
 };
 
 HashTable.prototype.retrieve = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  var arrayOfTupils = this._storage.get(index)
+  for (var i = 0; i < arrayOfTupils.length; i++) {
+    if (arrayOfTupils[i][0] === k) {
+      return arrayOfTupils[i][1];
+    }
+  }
 };
 
 HashTable.prototype.remove = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  this._storage.get(index).splice(0, 1);
 };
-
-
 
 /*
  * Complexity: What is the time complexity of the above functions?
  */
-
-
