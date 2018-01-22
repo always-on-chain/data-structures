5efb30d40829c701efe74bb62d5dd2169e325f05
waynekwest88 <waynekwest88@gmail.com>
2018-01-21 21:39:41 -0800

    completd binarySearchTree

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -1,5 +1,59 @@
 var BinarySearchTree = function(value) {
+    var tree = {
+       left:null,
+       right:null,
+       value:value
+    };
+    _.extend(tree, methods);
+    return tree;
 };
+
+var methods = {
+  insert: function(value) {
+    var node = BinarySearchTree(value);
+    if (this.value > node.value) {
+      if (this.left === null) {
+        this.left = node;
+      } else {
+        this.left.insert(value);
+      }
+    } else {
+      if (this.right === null) {
+        this.right = node;
+      } else {
+        this.right.insert(value);
+      }
+    }
+  }
+  ,
+  contains: function(value) {
+    var node
+    if (this.value === value){return true};
+    if (this.left === null && this.right === null) {
+       return false;
+    }
+    if (this.value > value){
+        node = this.left;
+    }else {
+        node = this.right;
+    }
+    return node.contains(value) ;
+  },
+
+  depthFirstLog: function(f) {
+    f(this.value);
+
+      if (this.left) {
+        this.left.depthFirstLog(f);
+      }
+      if (this.right) {
+        this.right.depthFirstLog(f);
+      }
+    }
+}
+
+
+
 
 
 /*
