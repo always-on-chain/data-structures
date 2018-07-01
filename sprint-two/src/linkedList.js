39bee16079c0b5f4de94a0f8e8676fcf17f46ad1
waynekwest88@gmail.com <waynekwest88@gmail.com>
2018-06-30 21:10:54 -0700

    completed linkedList exercises

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -4,12 +4,38 @@ var LinkedList = function() {
   list.tail = null;
 
   list.addToTail = function(value) {
+    var node = new Node(value);
+
+    if (list.head === null && list.tail === null) {
+      list.head = node;
+      list.tail = node;
+    } else if (list.head && list.tail === null) {
+      list.tail = node;
+      list.head.next = list.tail;
+    } else {
+      list.tail.next = node;
+      list.tail = node;
+    }
   };
 
   list.removeHead = function() {
+    var valueOfFormerHead = list.head.value;
+    list.head = list.head.next;
+    return valueOfFormerHead;
   };
 
   list.contains = function(target) {
+    var node = list.head;
+
+    while (node) {
+      if (node.value === target) {
+        return true;
+      } else {
+        node = node.next;
+      }
+    }
+
+    return false;
   };
 
   return list;
